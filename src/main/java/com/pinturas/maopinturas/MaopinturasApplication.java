6ff8726b7f28345f4c592b4d32f078b2edb5a399
Kevin Montealegre <kmontealegremelo@gmail.com>
2022-04-21 21:14:12 -0500

    Initial commit

---
+++ src/main/java/com/pinturas/maopinturas/MaopinturasApplication.java
@@ -0,0 +1,9 @@
+package com.pinturas.maopinturas;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MaopinturasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MaopinturasApplication.class, args);
+    }
+}
---
+++ src/test/java/com/pinturas/maopinturas/MaopinturasApplicationTests.java
@@ -0,0 +1,9 @@
+package com.pinturas.maopinturas;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class MaopinturasApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
