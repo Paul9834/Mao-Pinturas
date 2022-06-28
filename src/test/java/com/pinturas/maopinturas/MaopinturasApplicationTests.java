dde9f5218e475281ec4a451fe5a4f3c988d9cdb7
BRAYANDZ <BRDAZAFL@POLIGRAN.EDU.CO>
2022-06-27 22:40:52 -0500

    BRAYANDZ
    Limpieza de proyecto

--- src/test/java/com/pinturas/maopinturas/MaopinturasApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.pinturas.maopinturas;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest
-class MaopinturasApplicationTests {
-    @Test
-    void contextLoads() {
-    }
-}
